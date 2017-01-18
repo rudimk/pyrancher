49635be2943ecf4cad9b9f52824eec258dacc592
Rudraksh MK <rudraksh@paperkite.in>
2017-01-18 18:08:21 +0530

    Added a function to pull host details, for #1.

--- pyrancher/module.py
+++ pyrancher/module.py
@@ -2,6 +2,7 @@
 
 import requests as r
 import config
+import collections
 
 
 # Define a Rancher instance, which contains a list of available projects.
@@ -28,6 +29,7 @@ class Project:
     images = None
     state = None
     health = None
+    links = None
 
     def __init__(self, project_id):
         project_url = """{}/projects/{}""".format(config.RANCHER_INSTANCE, project_id)
@@ -38,4 +40,31 @@ class Project:
         self.name = raw_response['name']
         self.state = raw_response['state']
         self.health = raw_response['healthState']
+        self.links = raw_response['links']
+    
+    def getHosts(self):
+        api_key = config.RANCHER_API_KEY
+        api_secret = config.RANCHER_SECRET_KEY
+        raw_response = r.get(self.links['hosts'], auth=(api_key, api_secret)).json()
+        hosts = []
+        for host in raw_response['data']:
+            payload = collections.OrderedDict()
+            payload['uuid'] = host['uuid']
+            payload['id'] = host['id']
+            payload['name'] = host['name']
+            payload['state'] = host['state']
+            payload['ip'] = host['agentIpAddress']
+            payload['totalMemory'] = host['info']['memoryInfo']['memTotal']
+            payload['availableMemory'] = host['info']['memoryInfo']['memAvailable']
+            payload['os'] = host['info']['osInfo']['operatingSystem']
+            ports = []
+            for point in host['publicEndpoints']:
+                ports.append(point['port'])
+            payload['openPorts'] = ports
+            hosts.append(payload)
+        self.hosts = hosts
+
+
+
+
 
