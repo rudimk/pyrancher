13aff44ec189301313b9c47925a95a670938b6d9
Rudraksh MK <rudraksh@paperkite.in>
2017-01-18 14:18:34 +0530

    Added a class for representing a Rancher instance, and all projects available within it.

--- pyrancher/module.py
+++ pyrancher/module.py
@@ -1,1 +1,28 @@
 # Everything goes here.
+
+import requests as r
+import config
+
+
+# Define a Rancher instance, which contains a list of available projects.
+
+class Instance:
+
+    projects = None
+
+    def __init__(self):
+        projects_url = """{}/projects""".format(config.RANCHER_INSTANCE)
+        api_key = config.RANCHER_API_KEY
+        api_secret = config.RANCHER_SECRET_KEY
+        raw_response = r.get(projects_url, auth=(api_key, api_secret)).json()
+        self.projects = raw_response['data']
+
+# Define a Rancher project.
+
+class Project:
+
+    project_id = None
+    hosts = None
+    containers = None
+    images = None
+    status = None
